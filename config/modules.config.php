fb36a7388430296ae073c9689cec8b6f4f44ec5b
Joey Smith <jsmith@webinertia.net>
2022-08-08 22:20:51 -0500

    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- config/modules.config.php
+++ config/modules.config.php
@@ -6,6 +6,13 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Laminas\Form',
+    'Laminas\I18n',
+    'Laminas\InputFilter',
+    'Laminas\Filter',
+    'Laminas\Session',
+    'Laminas\Db',
+    'Laminas\Hydrator',
     'Laminas\Di',
     'Laminas\Router',
     'Laminas\Validator',
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -13,13 +13,21 @@ class ComposerStaticInita3244d27a7e85d1879656afd83deaa90
     public static $prefixLengthsPsr4 = array (
         'W' => 
         array (
+            'Webmozart\\Assert\\' => 17,
             'Webimpress\\SafeWriter\\' => 22,
+            'WebimpressCodingStandard\\' => 25,
+        ),
+        'S' => 
+        array (
+            'SlevomatCodingStandard\\' => 23,
         ),
         'P' => 
         array (
             'Psr\\Log\\' => 8,
             'Psr\\Container\\' => 14,
+            'Psr\\Cache\\' => 10,
             'PhpParser\\' => 10,
+            'PHPStan\\PhpDocParser\\' => 21,
         ),
         'L' => 
         array (
@@ -28,23 +36,37 @@ class ComposerStaticInita3244d27a7e85d1879656afd83deaa90
             'Laminas\\Uri\\' => 12,
             'Laminas\\Stdlib\\' => 15,
             'Laminas\\SkeletonInstaller\\' => 26,
+            'Laminas\\Session\\' => 16,
             'Laminas\\ServiceManager\\' => 23,
             'Laminas\\Router\\' => 15,
+            'Laminas\\Permissions\\Acl\\' => 24,
             'Laminas\\Mvc\\' => 12,
             'Laminas\\ModuleManager\\' => 22,
             'Laminas\\Loader\\' => 15,
             'Laminas\\Json\\' => 13,
+            'Laminas\\InputFilter\\' => 20,
+            'Laminas\\I18n\\' => 13,
+            'Laminas\\Hydrator\\' => 17,
             'Laminas\\Http\\' => 13,
+            'Laminas\\Form\\' => 13,
+            'Laminas\\Filter\\' => 15,
             'Laminas\\EventManager\\' => 21,
             'Laminas\\Escaper\\' => 16,
+            'Laminas\\Dom\\' => 12,
             'Laminas\\Di\\' => 11,
             'Laminas\\DevelopmentMode\\' => 24,
+            'Laminas\\Db\\' => 11,
             'Laminas\\Config\\' => 15,
             'Laminas\\ComponentInstaller\\' => 27,
+            'Laminas\\Authentication\\' => 23,
+            'LaminasCodingStandard\\' => 22,
         ),
         'D' => 
         array (
+            'Doctrine\\Common\\Lexer\\' => 22,
+            'Doctrine\\Common\\Annotations\\' => 28,
             'Di\\' => 3,
+            'Dealerdirect\\Composer\\Plugin\\Installers\\PHPCodeSniffer\\' => 55,
         ),
         'B' => 
         array (
@@ -58,9 +80,21 @@ class ComposerStaticInita3244d27a7e85d1879656afd83deaa90
     );
 
     public static $prefixDirsPsr4 = array (
+        'Webmozart\\Assert\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/webmozart/assert/src',
+        ),
         'Webimpress\\SafeWriter\\' => 
         array (
             0 => __DIR__ . '/..' . '/webimpress/safe-writer/src',
+        ),
+        'WebimpressCodingStandard\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/webimpress/coding-standard/src/WebimpressCodingStandard',
+        ),
+        'SlevomatCodingStandard\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/slevomat/coding-standard/SlevomatCodingStandard',
         ),
         'Psr\\Log\\' => 
         array (
@@ -70,9 +104,17 @@ class ComposerStaticInita3244d27a7e85d1879656afd83deaa90
         array (
             0 => __DIR__ . '/..' . '/psr/container/src',
         ),
+        'Psr\\Cache\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/cache/src',
+        ),
         'PhpParser\\' => 
         array (
             0 => __DIR__ . '/..' . '/nikic/php-parser/lib/PhpParser',
+        ),
+        'PHPStan\\PhpDocParser\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpstan/phpdoc-parser/src',
         ),
         'Laminas\\View\\' => 
         array (
@@ -94,6 +136,10 @@ class ComposerStaticInita3244d27a7e85d1879656afd83deaa90
         array (
             0 => __DIR__ . '/..' . '/laminas/laminas-skeleton-installer/src',
         ),
+        'Laminas\\Session\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/laminas/laminas-session/src',
+        ),
         'Laminas\\ServiceManager\\' => 
         array (
             0 => __DIR__ . '/..' . '/laminas/laminas-servicemanager/src',
@@ -101,6 +147,10 @@ class ComposerStaticInita3244d27a7e85d1879656afd83deaa90
         'Laminas\\Router\\' => 
         array (
             0 => __DIR__ . '/..' . '/laminas/laminas-router/src',
+        ),
+        'Laminas\\Permissions\\Acl\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/laminas/laminas-permissions-acl/src',
         ),
         'Laminas\\Mvc\\' => 
         array (
@@ -118,9 +168,29 @@ class ComposerStaticInita3244d27a7e85d1879656afd83deaa90
         array (
             0 => __DIR__ . '/..' . '/laminas/laminas-json/src',
         ),
+        'Laminas\\InputFilter\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/laminas/laminas-inputfilter/src',
+        ),
+        'Laminas\\I18n\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/laminas/laminas-i18n/src',
+        ),
+        'Laminas\\Hydrator\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/laminas/laminas-hydrator/src',
+        ),
         'Laminas\\Http\\' => 
         array (
             0 => __DIR__ . '/..' . '/laminas/laminas-http/src',
+        ),
+        'Laminas\\Form\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/laminas/laminas-form/src',
+        ),
+        'Laminas\\Filter\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/laminas/laminas-filter/src',
         ),
         'Laminas\\EventManager\\' => 
         array (
@@ -130,6 +200,10 @@ class ComposerStaticInita3244d27a7e85d1879656afd83deaa90
         array (
             0 => __DIR__ . '/..' . '/laminas/laminas-escaper/src',
         ),
+        'Laminas\\Dom\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/laminas/laminas-dom/src',
+        ),
         'Laminas\\Di\\' => 
         array (
             0 => __DIR__ . '/..' . '/laminas/laminas-di/src',
@@ -137,6 +211,10 @@ class ComposerStaticInita3244d27a7e85d1879656afd83deaa90
         'Laminas\\DevelopmentMode\\' => 
         array (
             0 => __DIR__ . '/..' . '/laminas/laminas-development-mode/src',
+        ),
+        'Laminas\\Db\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/laminas/laminas-db/src',
         ),
         'Laminas\\Config\\' => 
         array (
@@ -146,9 +224,29 @@ class ComposerStaticInita3244d27a7e85d1879656afd83deaa90
         array (
             0 => __DIR__ . '/..' . '/laminas/laminas-component-installer/src',
         ),
+        'Laminas\\Authentication\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/laminas/laminas-authentication/src',
+        ),
+        'LaminasCodingStandard\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/laminas/laminas-coding-standard/src/LaminasCodingStandard',
+        ),
+        'Doctrine\\Common\\Lexer\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/doctrine/lexer/lib/Doctrine/Common/Lexer',
+        ),
+        'Doctrine\\Common\\Annotations\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/doctrine/annotations/lib/Doctrine/Common/Annotations',
+        ),
         'Di\\' => 
         array (
             0 => __DIR__ . '/../..' . '/module/Di/src',
+        ),
+        'Dealerdirect\\Composer\\Plugin\\Installers\\PHPCodeSniffer\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/dealerdirect/phpcodesniffer-composer-installer/src',
         ),
         'Brick\\VarExporter\\' => 
         array (
